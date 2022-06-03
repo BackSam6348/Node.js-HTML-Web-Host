cf8efef47def916e2a362f95b71e454c20f7ce8c
POWERHELL <47339652+BackSam6348@users.noreply.github.com>
2022-06-03 09:26:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var fs = require('fs');
 var app = express();
 
 app.get('/', function (req, res) {
- fs.readFile('사용자 지정 이름.html', function (error, data) {
+ fs.readFile('#Your HTML File name.html', function (error, data) {
  res.writeHead(200, { 'Content-Type': 'text/html' });
  res.end(data);
  });
 });
-app.listen(포트, function () {
+app.listen(#Your Custom Port Number, function () {
  console.log('Load HTML');
 });
